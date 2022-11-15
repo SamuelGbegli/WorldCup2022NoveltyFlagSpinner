71899f178fe3b8675a3a3e40b60f74ea8d923240
sg563 <sg563@student.le.ac.uk>
2022-11-15 00:12:03 +0000

    initial commit

--- Source code/obj/Release/net6.0-windows/MainWindow.g.i.cs
+++ Source code/obj/Release/net6.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace WorldCup2022 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WorldCup2022;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WorldCup2022;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
